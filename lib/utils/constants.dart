fa2fefff2eeeb34cf8d68b14efd73e474a6e2bd3
Nemesislaw <narayangupta619@gmail.com>
2022-03-04 23:14:32 +0530

    first commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: non_constant_identifier_names
+
+String infura_url =
+    "https://kovan.infura.io/v3/0565d503cf0e41fea00bb81eef366306";
+
+String owner_private_key =
+    "a343ef824089b5935346503a0b10c89fd1d21d57d7643ba2bf872bf9e8a764d5";
+
+String voter_private_key =
+    "b091cba227dca9f63998890cdaced0374b885abbb7cdf158a974794d5561e1f0";
+
+String contractAddress1 = "0x3CB96d754Ce7479Bb3EF7810c1054D4Eb37b5cbB";
