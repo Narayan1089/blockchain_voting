69946cada20f6734faa06bbe834bb1d6472d6690
TusharGharge <tushargharge1111@gmail.com>
2022-03-05 04:47:09 +0530

    login functionallity

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,68 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:blockchain_voting/provider/candiateproviders.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'pages/home.dart';
+import 'screens/login_screen.dart';
 import 'utils/colors.dart';
 import 'widgets/bottomnavigation.dart';
 // import 'package:voting_dapp/pages/home.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: NavigateScreen(),///Home(),
-      debugShowCheckedModeBanner: false,
-     theme: ThemeData.dark().copyWith(
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: candiatesprovider(),
+        ),
+
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Honesty3.0',
+        theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: mobileBackgroundColor,
         ),
-    );
+           home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              // Checking if the snapshot has any data or not
+              if (snapshot.hasData) {
+                // if snapshot has data which means user is logged in then we check the width of screen and accordingly display the screen layout
+                return const NavigateScreen();
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('${snapshot.error}'),
+                );
+              }
+            }
+
+            // means connection to future hasnt been made yet
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return const LoginScreen();
+          },
+        ),
+
+    ),);
   }
 }
--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,4 +1,8 @@
+import 'package:blockchain_voting/resources/auth_methods.dart';
+import 'package:blockchain_voting/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+
+import '../utils/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({ Key? key }) : super(key: key);
@@ -10,8 +14,55 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+     return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        shadowColor: Colors.grey,
+        backgroundColor: mobileBackgroundColor,
+        title: const Text(
+          "Honesty3.0",
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: white, fontSize: 25),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.logout,
+              color: white,
+              size: 30.0,
+            ),
+            onPressed: () async {
+              await AuthMethods().signOut();
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (context) => const LoginScreen(),
+                ),
+              );
+            },
+          )
+        ],
+      ),
 
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            // ignore: prefer_const_constructors
+            child: Center(
+              child: const Text(
+                'Welcome User',
+                // ignore: unnecessary_const
+                style: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          // Expanded(child: Image.asset('assets/doctor_result.png')),
+        ],
+      ),
     );
   }
 }
